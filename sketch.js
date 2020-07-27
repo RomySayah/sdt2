86a0796e191f3e17a9247f6730bb75d4e31ef36d
Romy <romysayah@gmail.com>
2020-07-27 16:51:59 -0400

    shapediver scripts

--- sketch.js
+++ sketch.js
@@ -1,8 +1,5 @@
 // This example is compatible with any ShapeDiver model ticket.
 
-
-$.noConflict();
-
 let inputImg, currentImg, inputCanvas, output, statusMsg, pix2pix, transferBtn, modelReady = false, isTransfering = false; 
 
 let annotationToggle = true;
