b6e54e472a356bd8c3367d67af8a8a6ef5a98ce7
Romy <romysayah@gmail.com>
2020-07-27 15:18:27 -0400

    new stuff

--- sketch.js
+++ sketch.js
@@ -1,70 +1,3 @@
-let inputImg, currentImg, inputCanvas, output, statusMsg, pix2pix, transferBtn, modelReady = false, isTransfering = false; 
-
-let annotationToggle = true;
-        
-var canvas = document.getElementById('canvas');
-
-function setup(){   
-    output = select('#output');
-    statusMsg = select('#status');
-    transferBtn = select('#transferBtn');
-    
-    pixelDensity(1);
-    pix2pix = ml5.pix2pix('models/model-8800.meta', modelLoaded);
-}
-
-function draw(video, context, width, height){
-    var image, data, i, r1, g1, b1;
-    image = context.getImageData(0,0,width,height);
-    data = image.data;
-            
-    context.beginPath();    
-    context.fillStyle = "rgb(0,0,0)";
-    context.fillRect(0, 0, 512, 512);
-}
-
-function modelLoaded() { 
-    modelReady = true;
-    statusMsg.html('Model Loaded!');
-    transfer();
-    transferBtn.mousePressed(function(){ 
-        var inputData = context.toDataURL();
-        inputData.class('border-box').parent('input');   
-        inputImg = loadImage(inputData, drawImage);
-        currentImg = inputImg;
-        transfer(); 
-    });   
-}
-    
-function drawImage() {
-    image(inputImg,0,0, 256, 256);
-}
-
-function transfer() {
-    statusMsg.html('Transfering...');
-    isTransfering = true;
-    const canvasElement = select('canvas').elt;
-    // Apply pix2pix transformation
-    pix2pix.transfer(canvasElement, function(err, result) {
-        if (err) {
-            console.log(err);
-        }
-        if (result && result.src) {
-            statusMsg.html('Done!');
-            isTransfering = false;    
-            output.elt.src = result.src;
-        }
-    });
-}
-
-
-
-
-
-
-
-
-
 
 
 // This example is compatible with any ShapeDiver model ticket.
@@ -208,4 +141,89 @@ if (document.readyState === "loading") {
 } else {
   initSdvApp();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let inputImg, currentImg, inputCanvas, output, statusMsg, pix2pix, transferBtn, modelReady = false, isTransfering = false; 
+
+let annotationToggle = true;
+        
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+function setup(){   
+    output = select('#output');
+    statusMsg = select('#status');
+    transferBtn = select('#transferBtn');
+    
+    pixelDensity(1);
+    pix2pix = ml5.pix2pix('models/model-8800.meta', modelLoaded);
+}
+
+function draw(video, context, width, height){
+    var image, data;
+
+    image = context.getImageData(0,0,width,height);
+    data = image.data;
+            
+    context.beginPath();    
+    context.fillStyle = "rgb(0,0,0)";
+    context.fillRect(0, 0, 512, 512);
+}
+
+function modelLoaded() { 
+    modelReady = true;
+    statusMsg.html('Model Loaded!');
+    transfer();
+    transferBtn.mousePressed(function(){ 
+        var inputData = context.toDataURL();
+        inputData.class('border-box').parent('input');   
+        inputImg = loadImage(inputData, drawImage);
+        currentImg = inputImg;
+        transfer(); 
+    });   
+}
+    
+function drawImage() {
+    image(inputImg,0,0, 256, 256);
+}
+
+function transfer() {
+    statusMsg.html('Transfering...');
+    isTransfering = true;
+    const canvasElement = select('canvas').elt;
+    // Apply pix2pix transformation
+    pix2pix.transfer(canvasElement, function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+        if (result && result.src) {
+            statusMsg.html('Done!');
+            isTransfering = false;    
+            output.elt.src = result.src;
+        }
+    });
+}
+
+
+
 
