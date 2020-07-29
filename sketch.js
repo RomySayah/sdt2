3ab60d86436f1c6595352bdaf18bc42c5f6c4c74
Romy <romysayah@gmail.com>
2020-07-29 11:56:56 -0400

    sdv location

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let inputImg, currentImg, inputCanvas, output, statusMsg, pix2pix, transferBtn, 
 let annotationToggle = true;
         
 
-var canvas = document.getElementById('canvas');
+var canvas = document.getElementById('sdv-container');
 var context = canvas.getContext('2d');
 
 
@@ -205,7 +205,7 @@ function drawImage() {
 function transfer() {
     statusMsg.html('Transfering...');
     isTransfering = true;
-    const canvasElement = select('canvas').elt;
+    const canvasElement = select('sdv-container').elt;
     // Apply pix2pix transformation
     pix2pix.transfer(canvasElement, function(err, result) {
         if (err) {
