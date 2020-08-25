a4f15d9b39f0bc9a7a945b08b3fd8aeab02d4915
Romy <romysayah@gmail.com>
2020-08-24 22:21:21 -0400

    xx

--- sketch.js
+++ sketch.js
@@ -203,7 +203,11 @@ function transfer() {
 
 
 
-
-html2canvas(document.querySelector("#capture")).then(canvas => {
-  document.body.appendChild(canvas)
-})
+document.querySelector('#capture').addEventListener('click', function() {
+  html2canvas(document.querySelector('.specific'), {
+      onrendered: function(canvas) {
+          // document.body.appendChild(canvas);
+        return Canvas2Image.saveAsPNG(canvas);
+      }
+  });
+});
